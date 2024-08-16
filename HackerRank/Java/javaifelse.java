cc2f91b4124922dc4b3ccc30ffc9ef48fcbc751c
Shivani Sinha <147588548+chiragha@users.noreply.github.com>
2024-08-16 16:33:08 +0530

    Add files via upload

---
+++ HackerRank/Java/javaifelse.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class javaifelse { 
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        if(n%2 != 0){
+            System.out.println("Weird");
+        }else{
+            if(n>=6 && n<=20){
+                System.out.println("Weird");
+            }else{
+                System.out.println("Not Weird");
+            }
+        }
+    }
+}
